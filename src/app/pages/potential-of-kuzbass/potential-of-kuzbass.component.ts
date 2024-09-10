bb9b73d07a10e9fcf606e280e629f03059726d5a
Roman Mikaelyan <romanmik@yandex.ru>
2024-09-10 15:57:35 +0300

    feat: добавлено Потенциал Кузбасса

--- src/app/pages/potential-of-kuzbass/potential-of-kuzbass.component.ts
+++ src/app/pages/potential-of-kuzbass/potential-of-kuzbass.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
+import { Component, inject } from '@angular/core'
 import { BreadcrumbsComponent } from '@core/shared/ui/breadcrumbs/breadcrumbs.component'
 import { PageTitleComponent } from '@core/shared/ui/page-title/page-title.component'
 import { WrapperComponent } from '@core/wrapper/wrapper.component'
+import { PotentialOfKuzbassService } from './potential-of-kuzbass.service'
 
 @Component({
   selector: 'bb-potential-of-kuzbass',
   standalone: true,
-  imports: [WrapperComponent, BreadcrumbsComponent, PageTitleComponent],
+  imports: [
+    CommonModule,
+    WrapperComponent,
+    BreadcrumbsComponent,
+    PageTitleComponent,
+    NgOptimizedImage,
+  ],
   templateUrl: './potential-of-kuzbass.component.html',
   styleUrl: './potential-of-kuzbass.component.scss',
 })
 export class PotentialOfKuzbassComponent {
+  private pokService = inject(PotentialOfKuzbassService)
+
+  public regionAdvantages = this.pokService.regionAdvantages
+
   public items = [
     { route: '/', label: 'Главная' },
     { label: 'Потенциал Кузбасса' },
---
+++ src/app/pages/potential-of-kuzbass/potential-of-kuzbass.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { Observable, of } from 'rxjs'
+import { regionAdvantages } from './potential-of-kuzbass'
+import { toSignal } from '@angular/core/rxjs-interop'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PotentialOfKuzbassService {
+  private fetchRegionAdvantages(): Observable<string[]> {
+    return of(regionAdvantages)
+  }
+
+  public regionAdvantages = toSignal(this.fetchRegionAdvantages(), {
+    initialValue: [],
+  })
+}
